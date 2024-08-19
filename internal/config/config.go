b99b94be9ab7cb78744d51c8e76c0d1537b63fe0
djeddi-yacine <yacine2700dj@gmail.com>
2024-08-19 02:38:40 +0100

    load env as config

---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Config struct {
+	MongoDB  string
+	Proxy    string
+	CertFile string
+	KeyFile  string
+}
+
+func Load(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var (
+		config  = new(Config)
+		scanner = bufio.NewScanner(file)
+		comment = regexp.MustCompile(`\s*#.*$|^\s+|\s+$`)
+	)
+
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if !strings.Contains(txt, "=") {
+			continue
+		}
+
+		line := strings.TrimSpace(comment.ReplaceAllString(txt, ""))
+		kv := strings.SplitN(line, "=", 2)
+		if len(kv) < 1 {
+			continue
+		}
+
+		key := strings.TrimSpace(kv[0])
+		value := strings.Trim(strings.Trim(strings.TrimSpace(kv[1]), `"`), `'`)
+
+		switch key {
+		case "MONGO_DB_URL":
+			if value == "" {
+				return nil, errors.New("you need to set the Mongo DB url")
+			}
+			config.MongoDB = value
+		case "PROXY_URL":
+			config.Proxy = value
+		case "CERT_FILE":
+			config.CertFile = value
+		case "KEY_FILE":
+			config.KeyFile = value
+		}
+	}
+
+	return config, nil
+}
